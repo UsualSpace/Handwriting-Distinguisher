dcecb75a4ac47c2beacc7e76342673decea2fe97
UsualSpace <70238647+UsualSpace@users.noreply.github.com>
2025-04-22 00:25:28 -0700

    starting sample segmenter and preprocessor code

---
+++ src/sample_preprocessor.py
@@ -0,0 +1,6 @@
+# Filename: sample_preprocessor.py
+# Programmer(s): Abdurrahman Alyajouri
+# Date: 4/22/2025
+# Purpose: The purpose of this program is to preprocess a sentence image sample
+#          to clean up noise as best as possible, desaturate the image, and more, 
+#          in order to feed the neural network models only absolutely necessary information.
---
+++ src/sample_segmenter.py
@@ -0,0 +1,7 @@
+# Filename: sample_segmenter.py
+# Programmer(s): Abdurrahman Alyajouri
+# Date: 4/22/2025
+# Purpose: The purpose of this program is to take our raw handwriting sample pages
+#          (5 sentences per page), and segment each page to create samples containing
+#          a single sentence.
+
